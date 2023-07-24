53de4adf190466960d5b03cecca966c8e3c665bc
Ste-fis <140452999+Ste-fis@users.noreply.github.com>
2023-07-24 14:12:12 -0700

    Create hello.py
    Startup program for playing audio on computer startup.

---
+++ hello.py
@@ -0,0 +1,13 @@
+import os
+import sys
+#This is a simple script that will call the OS to open the specified file, in this case an audio file
+#Replace "--" with audio file name
+os.startfile("--")
+
+#Program closes after specified duration() 
+sys.exit(7)
+
+#To use this script on startup;
+#Press Win + R to open Run window
+#Type "shell: startup" and press enter (remove quotes)
+#Copy and paste .py file into startup folder
